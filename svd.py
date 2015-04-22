aec0898ec8abd3d095a9175402de417fe25688f5
Philip Seger <seger.philip@gmail.com>
2015-04-22 10:45:16 -0400

    now outputs a puppy from our matrix

--- svd.py
+++ svd.py
@@ -4,10 +4,14 @@ import Image
 import sys
 
 def image_to_gray():
+  # Converts the image to grayscale
+  # returns the image
   image = Image.open('image.jpg').convert('LA')
   return image
 
 def store_pixel_values(gray):
+  # gets the value of the pixel at a coordinate
+  # returns all values with color value
   global LENGTH, WIDTH
   height, WIDTH = gray.size
   LENGTH = height * WIDTH
@@ -19,12 +23,16 @@ def store_pixel_values(gray):
   return pixel
 
 def return_single_vals(values):
+  # removes the color value of pixels and just leaves gray
+  # returns the values
   pixel = []
   for i in values:
     pixel.append(i[0])
   return pixel
 
 def re_matrix(pixel):
+  # converts the list of pixel values into proper matrix
+  # returns the matrices
   global LENGTH, WIDTH
   new_pixel = []
   line = []
@@ -35,14 +43,24 @@ def re_matrix(pixel):
         line = []
   return new_pixel
 
+def pixel_to_image(og_pixel):
+  # create an image from the pixel values found previously
+  # saves the new image
+  global WIDTH, LENGTH
+  image = Image.new('RGB', (WIDTH, WIDTH))
+  image.save('output.png')
+  pixels = image.load()
+  for i in range(WIDTH):
+    for j in range(WIDTH):
+      pixels[j,i] = (og_pixel[j][i], og_pixel[j][i], og_pixel[j][i])
+  image.save('output.png')
 
 if __name__ == '__main__':
   gray = image_to_gray()
   values = store_pixel_values(gray)
   pixel = return_single_vals(values)
+  # print pixel
   new_pixel = re_matrix(pixel)
-  # print len(new_pixel)
-  # print len(new_pixel[599])
-  # print new_pixel
+  pixel_to_image(new_pixel)
 
   np.savetxt('values.txt', new_pixel, fmt="%s")
